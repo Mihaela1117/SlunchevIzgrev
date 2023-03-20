2f3525f8065f34ae3418323b9f62186d0db8cd66
Achkata0 <ak26826051@edu1.pgzaimov.com>
2023-03-20 11:36:26 +0200

    Authorize views

--- SlunchevIzgrev/Controllers/ReservationsController.cs
+++ SlunchevIzgrev/Controllers/ReservationsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using SlunchevIzgrev.Data;
 
 namespace SlunchevIzgrev.Controllers
 {
+    [Authorize]
     public class ReservationsController : Controller
     {
         private readonly SlunchevIzgrevDbContext _context;
